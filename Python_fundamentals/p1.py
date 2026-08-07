03b97dcbb618d70d244ffe2f6ea112c12cc3464f
Devansh Rai <raidevansh90@gmail.com>
2026-08-08 00:58:09 +0530

    Refactor print statements to include full name

--- Python_fundamentals/p1.py
+++ Python_fundamentals/p1.py
@@ -1,10 +1,10 @@
 #name
 first_name=input("Enter first name: ")
 last_name=input("Enter last name: ")
-print(first_name+ " "+ last_name)
+print("Full name "+first_name+ " "+ last_name)
 #age
 age=input("Enter age: ")
-print("Tony's age is "+ age)
+print(first_name+" " "age is "+ age)
 #height
 height=input("Enter height: ")
-print("Tony's height is "+ height)
+print(first_name+" " "height is "+ height)
