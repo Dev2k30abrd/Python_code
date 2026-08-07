6fb9fc1069e59acccdd0b4d704076cd74aeb1693
Devansh Rai <raidevansh90@gmail.com>
2026-08-08 00:53:09 +0530

    Add files via upload

---
+++ Python_fundamentals/p1.py
@@ -0,0 +1,10 @@
+#name
+first_name=input("Enter first name: ")
+last_name=input("Enter last name: ")
+print(first_name+ " "+ last_name)
+#age
+age=input("Enter age: ")
+print("Tony's age is "+ age)
+#height
+height=input("Enter height: ")
+print("Tony's height is "+ height)
