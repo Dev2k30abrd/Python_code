416c86a33fe496b1898fd39e3aed3f99f7b0c006
Devansh Rai <raidevansh90@gmail.com>
2026-08-08 22:20:50 +0530

    Update p1.py

--- Python_fundamentals/p1.py
+++ Python_fundamentals/p1.py
@@ -1,10 +1,10 @@
 #name
-first_name=input("Enter first name: ")
-last_name=input("Enter last name: ")
+first_name=input("Enter first name:")
+last_name=input("Enter last name:")
 print("Full name "+first_name+ " "+ last_name)
 #age
-age=input("Enter age: ")
+age=input("Enter age:")
 print(first_name+" " "age is "+ age)
 #height
-height=input("Enter height: ")
+height=input("Enter height:")
 print(first_name+" " "height is "+ height)
