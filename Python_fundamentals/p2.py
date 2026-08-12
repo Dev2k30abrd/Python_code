22daa1f9402e2f05ad3723ab2cf019d073eff0d3
Devansh Rai <raidevansh90@gmail.com>
2026-08-12 17:08:58 +0530

    Update p2.py

--- Python_fundamentals/p2.py
+++ Python_fundamentals/p2.py
@@ -2,8 +2,8 @@
 a=int(input("Enter an integer number: "))
 b=float(input("Enter a decimal number: "))
 
-sum=a + b #Type conversion (implicit)
-print("sum is:", sum)
+sum_1=a + b #Type conversion (implicit)
+print("sum is:", sum_1)
 
 sum_2=(a+int(b)) #Type casting (explicit)
 print("sum is:", sum_2)
