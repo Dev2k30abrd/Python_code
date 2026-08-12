079da757ec5adc456e45dd2ca4bcddafeba47c0a
Devansh Rai <raidevansh90@gmail.com>
2026-08-12 23:12:46 +0530

    Add files via upload

---
+++ Python_fundamentals/p3.py
@@ -0,0 +1,17 @@
+product1= int(input("Enter price of product 1: "))
+product2= int(input("Enter price of product 2: "))
+product3= int(input("Enter price of product 3: "))
+
+total_bill= product1+product2+product3  #Total bill of all three products
+avg_price= total_bill/3  #Average price
+
+print("Total bill is: ",total_bill)
+print("Average price is: ",avg_price)
+
+name= input("Enter a superhero name: ")
+print(name[0]=='S' or name[0]=='s')  # check for presence at starting position
+
+
+
+
+
