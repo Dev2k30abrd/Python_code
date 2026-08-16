913ca47d5cd3763a5156dcb94b07b63f87134fdc
Devansh Rai <raidevansh90@gmail.com>
2026-08-16 23:40:57 +0530

    Reformat and restore number processing code

--- Python_fundamentals/p5.py
+++ Python_fundamentals/p5.py
@@ -1,10 +1,10 @@
 #even/odd number
-
 num= int(input("Enter a number: "))
 if num%2==0:
     print("Even number")
 else:
     print("Odd number")
+
 
 #table of a number
 num=int(input("Enter a number: "))
@@ -12,6 +12,7 @@ for i in range(1,11):
     p=num*i
     i+=1
     print(p)
+
 
 #factorial of a number
 p=1
