077c9b96d03531592a6b99f8d711b7c16784951a
Devansh Rai <raidevansh90@gmail.com>
2026-08-20 00:49:37 +0530

    Update p7.py

--- Python_fundamentals/p7.py
+++ Python_fundamentals/p7.py
@@ -2,4 +2,4 @@
 roll_no=[101,105,102,101,108,105,110]
 print(list(set(roll_no)))
 
-#
+#given empployee records in the form of the list of tuples where each tuple contains: (employee id, employee name, employee salary)
